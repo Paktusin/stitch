b6450c1f4d0ba9a8dbd5ec80c2a85ca214835b92
mikhail.davlichin <mikhail.davlichin@kaspersky.com>
2020-07-03 18:13:57 +0300

    Updates

---
+++ precache-manifest.5fe8378767aa5abac6c7cf4dde7711a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65cdc95760772dcbec76c939abc33612",
+    "url": "/stitch/index.html"
+  },
+  {
+    "revision": "81130f39161b23bfa60b",
+    "url": "/stitch/static/css/main.f27ab55f.chunk.css"
+  },
+  {
+    "revision": "90301d843e9f2b2bca2f",
+    "url": "/stitch/static/js/2.059e7d76.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/stitch/static/js/2.059e7d76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81130f39161b23bfa60b",
+    "url": "/stitch/static/js/main.2027452d.chunk.js"
+  },
+  {
+    "revision": "62424200748dac8f01dd",
+    "url": "/stitch/static/js/runtime-main.a1e2fac2.js"
+  }
+]);
